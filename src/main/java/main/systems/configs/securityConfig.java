ab7b237892e3eaa3246f4a359989d97aed62f7a1
dnWork <denial1@yandex.ru>
2022-12-14 15:49:54 +0300

    "Урок 12. Практика" removed "extends WebSecurityConfigurerAdapter"

--- src/main/java/main/systems/configs/securityConfig.java
+++ src/main/java/main/systems/configs/securityConfig.java
@@ -1,38 +1,58 @@
 package main.systems.configs;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import main.systems.persistence.services.ServiceUser;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
-@RequiredArgsConstructor
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+@Configuration
 @Slf4j
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public class securityConfig extends WebSecurityConfigurerAdapter {
+public class securityConfig {
+//public class securityConfig extends WebSecurityConfigurerAdapter {
     private ServiceUser serviceUser;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//    log.info("Auth provider");
+//        http.csrf().disable();
+//        http.authorizeRequests()
+//                .antMatchers("/**").authenticated()
+//                .antMatchers("/**", "/app/api/v1/shop/**").hasAnyRole("USER", "ADMIN")
+//                .antMatchers("/api/v1/shop/admin/**").hasAnyAuthority("RIGHT_ADMIN")
+//                .anyRequest().permitAll()
+//                .and()
+//                .formLogin()
+//                .and()
+//                .logout()
+//                .invalidateHttpSession(true)
+//                .deleteCookies("JSESSIONID");
+//    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         log.info("Auth provider");
-        http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/**").authenticated()
-                .antMatchers("/**", "/app/api/v1/shop/**").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/api/v1/shop/admin/**").hasAnyAuthority("RIGHT_ADMIN")
-                .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                .and()
-                .logout()
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID");
+        httpSecurity.csrf().disable();
+       return  httpSecurity.authorizeHttpRequests()
+               .antMatchers("/**").authenticated()
+               .antMatchers("/**", "/app/api/v1/shop/**").hasAnyRole("USER", "ADMIN")
+               .antMatchers("/api/v1/shop/admin/**").hasAnyAuthority("RIGHT_ADMIN")
+               .anyRequest().permitAll()
+               .and()
+               .formLogin()
+               .and()
+               .logout()
+               .invalidateHttpSession(true)
+               .deleteCookies("JSESSIONID")
+               .and()
+               .build();
     }
 
     @Bean
