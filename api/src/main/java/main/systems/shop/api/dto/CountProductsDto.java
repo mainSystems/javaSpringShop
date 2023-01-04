8504ba26be8466322da3a523dc696f86407944d5
DnMing <neverfaten@gmail.com>
2023-01-05 02:46:21 +0300

    Урок 5. Spring Cloud. Часть 1 - add something

---
+++ api/src/main/java/main/systems/shop/api/dto/CountProductsDto.java
@@ -0,0 +1,23 @@
+package main.systems.shop.api.dto;
+
+
+public class CountProductsDto {
+    private Long id;
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
