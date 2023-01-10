494642029746302fbb5e9c05e0b1c37edfe2e4fc
DnMing <neverfaten@gmail.com>
2023-01-11 00:29:35 +0300

    Урок 5. Spring Cloud. Часть 2 - added gateway-service(half) (front one place configuratio - not working)

---
+++ gateway/src/main/java/main/systems/shop/gateway/ShopGatewayApp.java
@@ -0,0 +1,11 @@
+package main.systems.shop.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopGatewayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopGatewayApp.class, args);
+    }
+}
